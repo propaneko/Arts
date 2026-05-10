7c6c6d9828d22c8b28c7d11a88529fee3e8163bf
IckleBickleBee <nuclearshotgunxxx@gmail.com>
2026-05-09 20:20:59 -0500

    Update COADoughFormingRecipe.cs

--- CoreOfArt/CoreOfArt/Systems/COADoughFormingRecipe.cs
+++ CoreOfArt/CoreOfArt/Systems/COADoughFormingRecipe.cs
@@ -49,7 +49,7 @@ namespace CoreOfArts.Systems
 
     return ok;
 }
-public override Dictionary<string, string[]> GetNameToCodeMapping(IWorldAccessor world)
+protected override Dictionary<string, string[]> GetNameToCodeMapping(IWorldAccessor world)
 {
     Dictionary<string, string[]> mappings = new Dictionary<string, string[]>();
 
